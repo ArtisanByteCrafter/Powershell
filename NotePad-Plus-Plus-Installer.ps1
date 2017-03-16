96fdb2665e67f6c3754f784497321a8936fcedf9
artvandelay440 <natethegreat440@gmail.com>
2017-03-16 13:20:00 -0500

    Added Notepad++ Installer

---
+++ NotePad-Plus-Plus-Installer.ps1
@@ -0,0 +1,15 @@
+﻿$url = 'https://notepad-plus-plus.org'
+$webresponse = Invoke-WebRequest https://notepad-plus-plus.org/download
+$installargs = '/S'
+
+# RestAPI invoke
+$download =  $url+ ($webresponse.Links | 
+    Where-Object {$_.innerText -like "*Notepad++ Installer 32-Bit x86*"} | 
+    Select-Object href -ExpandProperty href)
+
+# Download binary and install
+Invoke-WebRequest $download -UseBasicParsing -OutFile "$PSScriptRoot\Notepad-Plus-Plus-Latest-Installer.exe"
+Start-Process "$PSScriptRoot\Notepad-Plus-Plus-Latest-Installer.exe" -ArgumentList $installargs #-Wait
+
+#Uncomment the -Wait flag above and the command below to remove binary after install
+# Remove-Item "$PSScriptRoot\Notepad-Plus-Plus-Latest-Installer.exe"

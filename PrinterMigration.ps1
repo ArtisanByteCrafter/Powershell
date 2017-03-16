9657218539069f629c49ffedc915779a2c945fe3
artvandelay440 <natethegreat440@gmail.com>
2017-03-16 13:49:28 -0500

    Update PrinterMigration.ps1

--- PrinterMigration.ps1
+++ PrinterMigration.ps1
@@ -1,6 +1,6 @@
 $newPrintServer = 'newprintserver'
 $oldPrintServer = 'oldprintserver'
-
+ 
 $PrinterMap = @{
 "\\$oldPrintServer\old-printer-name01" = "\\$newPrintServer\new-printer-name-01"
 "\\$oldPrintServer\old-printer-name02" = "\\$newPrintServer\new-printer-name-02"
@@ -10,32 +10,32 @@ $PrinterMap = @{
 "\\$oldPrintServer\old-printer-name06" = "\\$newPrintServer\new-printer-name-06"
 "\\$oldPrintServer\old-printer-name07" = "\\$newPrintServer\new-printer-name-07"
 }
-
+ 
 # get old default printer
-$Default = Get-WmiObject -Query " SELECT * FROM Win32_Printer WHERE Default=$true"
-
+$oldDefault = Get-WmiObject -Class Win32_Printer | Where-Object {$_.Default -eq $True}
+ 
 # determine if there are any mapped printers on old server
 $printers = @(Get-WmiObject -Class Win32_Printer -Filter "SystemName='\\\\$oldPrintServer'" -ErrorAction Stop)
-
+ 
 # maps new printer based on hash literal defined above. Default printer is preserved.
 If ($printers.count -gt 0) {
     ForEach ($printer in $printers) {
-        If ($Default.Name -eq $printer.Name){
+        If ($oldDefault.Name -eq $printer.Name){
             $newPrinter = $PrinterMap[$printer.Name]
             if($newprinter){
-                $defaultValue = ([wmiclass]"Win32_Printer").AddPrinterConnection($newPrinter).ReturnValue
-                $e = $newPrinter.Substring(16)
-                $a = Get-WMIObject -query "Select * From Win32_Printer Where ShareName = '$e'"
-                $a.SetDefaultPrinter()
-                Start-Sleep -Seconds 120
+                ([wmiclass]"Win32_Printer").AddPrinterConnection($newPrinter).ReturnValue
+                $e = $newPrinter.split('\')[3]               
+                $newDefault = Get-WmiObject -Class Win32_Printer | Where-Object {$_.ShareName -eq "$e"}
+                $newDefault.SetDefaultPrinter()
+                Start-Sleep -Seconds 30
                 $printer.Delete()
                 }
             }
         Else {
             $newPrinter = $PrinterMap[$printer.Name]
             if($newprinter){
-                $returnValue = ([wmiclass]"Win32_Printer").AddPrinterConnection($newPrinter).ReturnValue
-                Start-Sleep -Seconds 120
+                ([wmiclass]"Win32_Printer").AddPrinterConnection($newPrinter).ReturnValue
+                Start-Sleep -Seconds 30
                 $printer.Delete()
                 }
             }

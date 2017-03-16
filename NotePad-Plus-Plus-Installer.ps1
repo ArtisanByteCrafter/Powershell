b0be0704f2b3fe75f12bf77f11f6aa0e554a6196
artvandelay440 <natethegreat440@gmail.com>
2017-03-16 13:22:09 -0500

    Fixed errant comment

--- NotePad-Plus-Plus-Installer.ps1
+++ NotePad-Plus-Plus-Installer.ps1
@@ -2,7 +2,7 @@
 $webresponse = Invoke-WebRequest https://notepad-plus-plus.org/download
 $installargs = '/S'
 
-# RestAPI invoke
+# Parse web page for download string
 $download =  $url+ ($webresponse.Links | 
     Where-Object {$_.innerText -like "*Notepad++ Installer 32-Bit x86*"} | 
     Select-Object href -ExpandProperty href)
